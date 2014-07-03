532234082bd7291f89b0ea8af47bc0a14ce6455d
jleahy2 <jleahy2@gmail.com>
2014-07-03 15:39:59 -0500

    Create roll.php

---
+++ roll.php
@@ -0,0 +1,19 @@
+/**
+ * A simple dice roller CLI PHP application that accepts two parameters: the number of dice (1-1000) and the number of sides (2-100)
+ *
+ * The code attempts to handle and sanitize bad user input gracefully.
+ * 
+ * The string argument is in the following format: {digit}d{digit} 
+ * The first digit represents the number of dice and the second digit represents the number of sides. 
+ *
+ *
+ * CLI example rolling 3 dice that have 6 sides each:   
+ * bash: php roll.php 3d6
+ *
+ * returns: 
+ * bash: 18
+ *
+ *
+ *
+ */
+ 
